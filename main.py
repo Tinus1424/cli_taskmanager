c7486903361fa39ceaed6b51dbf0db61741e630b
Tinus1424 <martijnqvtz@outlook.com>
2025-10-06 20:46:31 +0200

    added a remove and update function

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-from src.tasks import add_task, list_tasks
+from src.tasks import add_task, list_tasks, remove_task, update_status
 import argparse
 
 
 
 def main():
     parser = argparse.ArgumentParser(description = "This is a taskmanager")
+    parser.add_argument("-v", "--verbose", help="print additional information", action="store_true")
     parser.add_argument("-a", "--add", help="add task")
     parser.add_argument("-l", "--list", help="list tasks", action="store_true")
+    parser.add_argument("-r", "--remove", help="remove tasks")
+    parser.add_argument("-u", "--update", help="update the status of a task")
+    parser.add_argument("-s", "--status", help="specificy a status for update")
     args = parser.parse_args()
-    if args.add: 
-        add_task(args.add)
-        print(f"Added '{args.add}' to the tasks list")
-    if args.list:
-        list_tasks()
 
+    if args.add: 
+        add_task(args.add, args.verbose)
+    if args.list:
+        list_tasks(args.verbose)
+    if args.remove:
+        remove_task(args.remove, args.verbose)
+    if args.update:
+        if not args.status:
+            print("Please specify a status using -t")
+            return
+        update_status(args.update, args.status, args.verbose)
 
 if __name__ == "__main__":
     main()
--- src/tasks.py
+++ src/tasks.py
@@ -2,7 +2,10 @@ import json
 import os
 
 
-def add_task(task):
+def add_task(task, verbose):
+    if not os.path.exists("data"):
+        os.mkdir("data")
+
     file_path = os.path.abspath("data/current_tasks.json")
     if not os.path.exists(file_path):
         with open(file_path, "w") as f:
@@ -10,18 +13,61 @@ def add_task(task):
 
     with open(file_path, "r") as f:
         tasks = json.load(f)
-    
-    tasks[task] = 0
+    if tasks[task]:
+        print(f"Task already in task list")
+        return
+    tasks[task] = "started"
 
+    
     with open(file_path, "w") as f:
         json.dump(tasks, f, indent = 4) 
 
-def list_tasks():
+    if verbose:
+        print(f"Succesfully added '{task}'(status: started) to the task list!")
+
+def list_tasks(verbose):
     file_path = os.path.abspath("data/current_tasks.json")
     if not os.path.exists(file_path):
-        return "There are currently no tasks in the list" 
+        if verbose:
+            return "There are currently no tasks in the list" 
+        else:
+            return
 
     with open(file_path, "r") as f:
         tasks = json.load(f)
 
-    print(tasks)
+    for k, v in tasks.items():
+        print(f"Task: {k}, status: {v}")
+
+def remove_task(task, verbose):
+    file_path = os.path.abspath("data/current_tasks.json")
+    if not os.path.exists(file_path):
+        return "There are currently no tasks in the list"
+
+    with open(file_path, "r") as f:
+        tasks = json.load(f)
+
+    del tasks[task]
+
+    with open(file_path, "w") as f:
+        json.dump(tasks, f, indent = 4)
+
+    if verbose:
+        print(f"Succesfully removed {task} from the task list!")
+
+def update_status(task, status, verbose):
+    file_path = os.path.abspath("data/current_tasks.json")
+    if not os.path.exists(file_path):
+        return "There are currently no tasks in the list"
+
+    with open(file_path, "r") as f:
+        tasks = json.load(f)
+
+    old_status = tasks.get(task)
+    tasks[task] = status
+
+    with open(file_path, "w") as f:
+        json.dump(tasks, f, indent = 4)
+
+    if verbose: 
+        print(f"Succesfully updated the status of {task} from {old_status} to {tasks.get(task)}")
